254a879c30f3dfb7c2a535b545d3dc10edb3680b
serial-projects <serialexperimentalchannel@gmail.com>
2024-09-21 12:01:40 -0300

    add progator - solar types conversions.

--- include/Solar/Types.hpp
+++ include/Solar/Types.hpp
@@ -6,6 +6,8 @@
 #include <unordered_map>
 #include <cstdint>
 
+#include "Progator/Types.hpp"
+
 namespace Solar
 {
     /* BASIC TYPES: */
@@ -22,6 +24,14 @@ namespace Solar
     typedef char            Character;
     typedef std::uint32_t   RGBA84;
     typedef std::string     String;
+
+    /* PROGATOR - SOLAR TYPES: */
+    typedef Progator::Matrix44 Matrix44;
+    typedef Progator::Matrix33 Matrix33;
+    typedef Progator::Matrix22 Matrix22;
+    typedef Progator::Vector4 Vector4;
+    typedef Progator::Vector3 Vector3;
+    typedef Progator::Vector2 Vector2;
 };
 
 #endif
