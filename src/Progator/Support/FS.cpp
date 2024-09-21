e759be27f26e2b829d8713bcf850145ea421def6
serial-projects <serialexperimentalchannel@gmail.com>
2024-09-21 11:06:47 -0300

    file buffer.

---
+++ src/Progator/Support/FS.cpp
@@ -0,0 +1,28 @@
+#include "Progator/Support/FS.hpp"
+
+#include <cstdio>
+#include <cstdlib>
+
+Progator::Support::FileBuffer FileBufferGet(const Progator::Character* path)
+{
+    std::FILE *file_pointer = std::fopen(path, "r");
+    Progator::Support::FileBuffer buffer = nullptr;
+    if(file_pointer != nullptr)
+    {
+        std::fseek(file_pointer, 0, SEEK_END);
+        Progator::U32 length = std::ftell(file_pointer);
+        buffer = new Progator::Character[length + 1];
+        
+        std::fseek(file_pointer, 0, SEEK_SET);
+        auto _A = std::fread(buffer, length, sizeof(Progator::Character), file_pointer);
+        
+        std::fclose(file_pointer);
+        buffer[length] = 0;
+    }
+    return buffer;
+}
+
+void FileBufferDestroy(Progator::Support::FileBuffer buffer)
+{
+    delete buffer;
+}
