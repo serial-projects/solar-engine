a6fc57937567f33fafcd73c1f329c1ccdaf65f3e
serial-projects <serialexperimentalchannel@gmail.com>
2024-09-21 13:08:15 -0300

    level concept.

---
+++ include/Solar/Scene/Level.hpp
@@ -0,0 +1,33 @@
+#ifndef SolarSceneLevel_hpp
+#define SolarSceneLevel_hpp
+
+#include "Solar/Types.hpp"
+#include "Solar/Core.hpp"
+
+#include "Solar/Scene/Camera.hpp"
+#include "Solar/Scene/Level.hpp"
+
+namespace Solar
+{
+    namespace Scene
+    {
+        namespace LevelProperties
+        {
+            /* TODO: implement this: */
+        };
+
+        typedef struct Level
+        {
+            Solar::Core* linked_core;
+        } Level;
+
+        Solar::Scene::Level* LevelNew();
+        void LevelDestroy(Solar::Scene::Level* level);
+        void LevelInit(Solar::Scene::Level* level, Solar::Core *core);
+        void LevelTick(Solar::Scene::Level* level);
+        void LevelDraw(Solar::Scene::Level* level);
+        void LevelQuit(Solar::Scene::Level* level);
+    };
+};
+
+#endif
---
+++ src/Solar/Scene/Level.cpp
@@ -0,0 +1,48 @@
+#include "Solar/Scene/Level.hpp"
+
+Solar::Scene::Level* Solar::Scene::LevelNew()
+{
+    Solar::Scene::Level* proto = new Solar::Scene::Level;
+    return proto;
+}
+
+void Solar::Scene::LevelDestroy(Solar::Scene::Level* level)
+{
+    delete level;
+}
+
+void Solar::Scene::LevelInit(Solar::Scene::Level* level, Solar::Core *core)
+{
+    level->linked_core = core;
+}
+
+void Solar::Scene::LevelTick(Solar::Scene::Level* level)
+{
+    /* NOTE: Basic event handler: */
+    SDL_Event os_event_handler;
+    while(SDL_PollEvent(&os_event_handler))
+    {
+        switch(os_event_handler.type)
+        {
+            case SDL_QUIT:
+                level->linked_core->state = 0;
+                break;
+            default:
+                break;
+        }
+    }
+
+    /* TODO: keep updating the scene: */
+}
+
+void Solar::Scene::LevelDraw(Solar::Scene::Level* level)
+{
+    /* TODO: implement should redraw to prevent drawing useless frames: */
+    Progator::RendererClear(level->linked_core->renderer, 0x00000000);
+    Progator::WindowDraw(level->linked_core->window);
+}
+
+void Solar::Scene::LevelQuit(Solar::Scene::Level* level)
+{
+    /* TODO: do saving and other crap here: */
+}
