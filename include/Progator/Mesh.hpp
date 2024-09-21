a9ba28fbb8a9f779d528baee2239e102dcc10aea
serial-projects <serialexperimentalchannel@gmail.com>
2024-09-21 11:04:28 -0300

    mesh implemented.

---
+++ include/Progator/Mesh.hpp
@@ -0,0 +1,23 @@
+#ifndef ProgatorMesh_hpp
+#define ProgatorMesh_hpp
+
+#include "Progator/Types.hpp"
+#include "Progator/Validator.hpp"
+#include "Progator/Pointers.hpp"
+
+namespace Progator
+{
+    typedef struct Mesh
+    {
+        void *backend;
+        Progator::Pointers* pointers;
+        Progator::Validator *validator;
+    } Mesh;
+    Progator::Mesh* MeshNew();
+    void MeshDestroy(Progator::Mesh* mesh);
+    void MeshInit(Progator::Mesh* mesh, Progator::Pointers* pointers, Progator::Validator* validator);
+    void MeshLoadVerTexNor(Progator::Mesh* mesh, Progator::F32 data[], const Progator::U32 amount);
+    void MeshDraw(Progator::Mesh* mesh);
+};
+
+#endif
---
+++ src/Progator/Mesh.cpp
@@ -0,0 +1,38 @@
+#include "Progator/Mesh.hpp"
+
+Progator::Mesh* Progator::MeshNew()
+{
+    Progator::Mesh* proto = new Progator::Mesh;
+    return proto;
+}
+
+void Progator::MeshDestroy(Progator::Mesh* mesh)
+{
+    mesh->pointers->mesh_destroy(mesh->backend);
+    delete mesh;
+}
+
+void Progator::MeshInit(Progator::Mesh* mesh, Progator::Pointers* pointers, Progator::Validator* validator)
+{
+    mesh->validator = validator;
+    mesh->pointers = pointers;
+    
+    mesh->backend = mesh->pointers->mesh_new();
+    mesh->pointers->mesh_init(mesh->backend, mesh->validator);
+}
+
+void Progator::MeshLoadVerTexNor(Progator::Mesh* mesh, Progator::F32 data[], const Progator::U32 amount)
+{
+    mesh->pointers->mesh_load_vertexnor(
+        mesh->backend,
+        data,
+        amount
+    );
+}
+
+void Progator::MeshDraw(Progator::Mesh* mesh)
+{
+    mesh->pointers->mesh_draw(
+        mesh->backend
+    );
+}
