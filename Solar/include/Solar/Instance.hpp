3cef454dc0a72e04c01ee500da0e4152c696ea09
serial-projects <serialexperimentalchannel@gmail.com>
2025-01-20 10:51:44 -0300

    include the tick, draw ... refactor.

--- Solar/include/Solar/Instance.hpp
+++ Solar/include/Solar/Instance.hpp
@@ -2,5 +2,9 @@
 #define SolarInstance_hpp
 
 #include "Solar/Instance/Structure.hpp"
+#include "Solar/Instance/Init.hpp"
+#include "Solar/Instance/Tick.hpp"
+#include "Solar/Instance/Loop.hpp"
+#include "Solar/Instance/Quit.hpp"
 
 #endif
