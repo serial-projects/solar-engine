9370202f1121390a193a3b7e0df5ff231ebfffe8
serial-projects <serialexperimentalchannel@gmail.com>
2025-01-21 11:46:42 -0300

    new graphic system for multiwindow engine.

--- Solar/include/Solar/Core/Graphics.hpp
+++ Solar/include/Solar/Core/Graphics.hpp
@@ -1,23 +1,7 @@
 #ifndef SolarCoreGraphics_hpp
 #define SolarCoreGraphics_hpp
 
-#include "Solar/Types.hpp"
-
-namespace Solar
-{
-    namespace Core
-    {
-        struct Graphics
-        {
-            Progator::Base::Pointers pointers;
-            Progator::Base::Window* window;
-            Progator::Base::Renderer* renderer;
-            Logica::Control::Validator validator;
-        };
-        Solar::Core::Graphics GraphicsNew();
-        void GraphicsDestroy(Solar::Core::Graphics* graphics);
-        void GraphicsInit(Solar::Core::Graphics* graphics);
-    };
-};
+#include "Solar/Core/Graphics/Unit.hpp"
+#include "Solar/Core/Graphics/Units.hpp"
 
 #endif
