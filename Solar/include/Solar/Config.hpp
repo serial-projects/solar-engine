d728af5d9d1fb29953f679d675b74517e392cf56
serial-projects <serialexperimentalchannel@gmail.com>
2025-01-21 11:47:08 -0300

    Add an configuration for future tuning.

--- Solar/include/Solar/Config.hpp
+++ Solar/include/Solar/Config.hpp
@@ -1,5 +1,16 @@
 #ifndef SolarConfig_hpp
 #define SolarConfig_hpp
 
+// General Information
+#define SOLAR_VERSION                   10000
+#define SOLAR_VERSION_STR               "1.0.000"
+#define SOLAR_VERSION_NAME              "Ghost"
+#define SOLAR_VERSION_FULL              "Solar RPG Engine, v." SOLAR_VERSION_STR ", or: \"" SOLAR_VERSION_NAME "\"" 
+
+// Solar/Modes/Scene/Components/Events.hpp:
+
+// SOLAR_MODES_SCENE_COMPONENTS_EVENTS_SIZE: defines the size of the
+// Solar::Modes::Scene::Components::Events vector, it holds an SDL_Event to the entire project.
+#define SOLAR_MODES_SCENE_COMPONENTS_EVENTS_SIZE                        UINT8_MAX
 
 #endif
