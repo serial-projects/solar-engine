7018663e03c67272db1a3466e478bb1435006b49
serial-projects <serialexperimentalchannel@gmail.com>
2025-01-23 10:57:18 -0300

    load the DataFile object.

---
+++ Solar/include/Solar/Core/Provider/Load/Datafile.hpp
@@ -0,0 +1,15 @@
+#ifndef SolarCoreProviderLoadDatafile_hpp
+#define SolarCoreProviderLoadDatafile_hpp
+
+namespace Solar
+{
+    namespace Core
+    {
+        namespace Provider
+        {
+            
+        };
+    };
+};
+
+#endif
