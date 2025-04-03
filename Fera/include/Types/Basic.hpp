06dd1c13511566090ac58aaf115cfa65f9a792b3
serial-projects <serialexperimentalchannel@gmail.com>
2025-04-03 20:17:56 -0300

    Begin Fera Implementation using Classes.

---
+++ Fera/include/Types/Basic.hpp
@@ -0,0 +1,31 @@
+#ifndef FeraTypesBasic_hpp
+#define FeraTypesBasic_hpp
+
+#include "Logica/Logica.hpp"
+#include <string>
+#include <vector>
+
+namespace Fera
+{
+    namespace Types
+    {
+        namespace Basic
+        {
+            typedef Logica::Types::Basic::U8        U8;
+            typedef Logica::Types::Basic::I8        I8;
+            typedef Logica::Types::Basic::U16       U16;
+            typedef Logica::Types::Basic::I16       I16;
+            typedef Logica::Types::Basic::U32       U32;
+            typedef Logica::Types::Basic::I32       I32;
+            typedef Logica::Types::Basic::U64       U64;
+            typedef Logica::Types::Basic::I64       I64;
+            typedef Logica::Types::Basic::F32       F32;
+            typedef Logica::Types::Basic::F64       F64;
+            typedef Logica::Types::Basic::CH8       CH8;
+            typedef Logica::Types::Basic::Result    Result;
+            typedef Logica::Types::Basic::String    String;
+        };
+    };
+};
+
+#endif
