c602764e86f012d8b1bd9af25a65aded579d4fe4
serial-projects <serialexperimentalchannel@gmail.com>
2025-04-04 10:01:47 -0300

    Refactor Buffer assignment functions using macros for cleaner code

--- Logica/include/Logica/Types/Stream/Buffer.hpp
+++ Logica/include/Logica/Types/Stream/Buffer.hpp
@@ -10,124 +10,117 @@ namespace Logica
     {
         namespace Stream
         {
-            namespace BufferFunctions
-            {
-                typedef std::function<void(void*)>                                      Init;
-                typedef std::function<void(void*, const Logica::Types::Basic::Size)>    SetPosition;
-                typedef std::function<Logica::Types::Basic::Size(void*)>                GetPosition;
-                typedef std::function<void(void*)>                                      Close;
-
-                /* Read Functions: */
-
-                typedef std::function<Logica::Types::Basic::U8(void*)>  ReadU8;
-                typedef std::function<Logica::Types::Basic::U16(void*)> ReadU16;
-                typedef std::function<Logica::Types::Basic::U32(void*)> ReadU32;
-                typedef std::function<Logica::Types::Basic::U64(void*)> ReadU64;
-                
-                typedef std::function<Logica::Types::Basic::I8(void*)>  ReadI8;
-                typedef std::function<Logica::Types::Basic::I16(void*)> ReadI16;
-                typedef std::function<Logica::Types::Basic::I32(void*)> ReadI32;
-                typedef std::function<Logica::Types::Basic::I64(void*)> ReadI64;
-            };
-
             class Buffer
             {
-                private:
-                Logica::Types::Stream::BufferFunctions::Init        init_f;
-                Logica::Types::Stream::BufferFunctions::SetPosition set_position_f;
-                Logica::Types::Stream::BufferFunctions::GetPosition get_position_f;
-                Logica::Types::Stream::BufferFunctions::Close       close_f;
-                Logica::Types::Stream::BufferFunctions::ReadU8      readu8_f;
-                Logica::Types::Stream::BufferFunctions::ReadU16     readu16_f;
-                Logica::Types::Stream::BufferFunctions::ReadU32     readu32_f;
-                Logica::Types::Stream::BufferFunctions::ReadU64     readu64_f;
-                Logica::Types::Stream::BufferFunctions::ReadI8      readi8_f;
-                Logica::Types::Stream::BufferFunctions::ReadI16     readi16_f;
-                Logica::Types::Stream::BufferFunctions::ReadI32     readi32_f;
-                Logica::Types::Stream::BufferFunctions::ReadI64     readi64_f;
-                void*                                               userdata;
-
                 public:
+                    /**
+                     * Set all the callback functions inside the class.
+                     */
+                    typedef std::function<void(void*)> InitFunction;
+                    typedef std::function<void(void*, const Logica::Types::Basic::Size)> SetPositionFunction;
+                    typedef std::function<Logica::Types::Basic::Size(void*)> GetPositionFunction;
+                    typedef std::function<void(void*)> CloseFunction;
+                    typedef std::function<Logica::Types::Basic::U8(void*)>  ReadU8Function;
+                    typedef std::function<Logica::Types::Basic::U16(void*)> ReadU16Function;
+                    typedef std::function<Logica::Types::Basic::U32(void*)> ReadU32Function;
+                    typedef std::function<Logica::Types::Basic::U64(void*)> ReadU64Function;
+                    typedef std::function<Logica::Types::Basic::I8(void*)>  ReadI8Function;
+                    typedef std::function<Logica::Types::Basic::I16(void*)> ReadI16Function;
+                    typedef std::function<Logica::Types::Basic::I32(void*)> ReadI32Function;
+                    typedef std::function<Logica::Types::Basic::I64(void*)> ReadI64Function;
 
-                /* Assign Functions: */
-                void AssignInit(
-                    Logica::Types::Stream::BufferFunctions::Init init_f
-                );
-                
-                void AssignSetPosition(
-                    Logica::Types::Stream::BufferFunctions::SetPosition set_position_f
-                );
+                    void AssignInit(
+                        Logica::Types::Stream::Buffer::InitFunction init
+                    );
+                    
+                    void AssignSetPosition(
+                        Logica::Types::Stream::Buffer::SetPositionFunction set_position
+                    );
 
-                void AssignGetPosition(
-                    Logica::Types::Stream::BufferFunctions::GetPosition get_position_f
-                );
-                
-                void AssignClose(
-                    Logica::Types::Stream::BufferFunctions::Close close_f
-                );
+                    void AssignGetPosition(
+                        Logica::Types::Stream::Buffer::GetPositionFunction get_position
+                    );
+                    
+                    void AssignClose(
+                        Logica::Types::Stream::Buffer::CloseFunction close
+                    );
 
-                void AssignReadU8(
-                    Logica::Types::Stream::BufferFunctions::ReadU8 readu8_f
-                );
+                    void AssignReadU8(
+                        Logica::Types::Stream::Buffer::ReadU8Function readu8
+                    );
 
-                void AssignReadU16(
-                    Logica::Types::Stream::BufferFunctions::ReadU16 readu16_f
-                );
+                    void AssignReadU16(
+                        Logica::Types::Stream::Buffer::ReadU16Function readu16
+                    );
 
-                void AssignReadU32(
-                    Logica::Types::Stream::BufferFunctions::ReadU32 readu32_f
-                );
+                    void AssignReadU32(
+                        Logica::Types::Stream::Buffer::ReadU32Function readu32
+                    );
 
-                void AssignReadU64(
-                    Logica::Types::Stream::BufferFunctions::ReadU64 readu64_f
-                );
+                    void AssignReadU64(
+                        Logica::Types::Stream::Buffer::ReadU64Function readu64
+                    );
 
-                void AssignReadI8(
-                    Logica::Types::Stream::BufferFunctions::ReadI8 readi8_f
-                );
+                    void AssignReadI8(
+                        Logica::Types::Stream::Buffer::ReadI8Function readi8
+                    );
 
-                void AssignReadI16(
-                    Logica::Types::Stream::BufferFunctions::ReadI16 readi16_f
-                );
+                    void AssignReadI16(
+                        Logica::Types::Stream::Buffer::ReadI16Function readi16
+                    );
 
-                void AssignReadI32(
-                    Logica::Types::Stream::BufferFunctions::ReadI32 readi32_f
-                );
+                    void AssignReadI32(
+                        Logica::Types::Stream::Buffer::ReadI32Function readi32
+                    );
 
-                void AssignReadI64(
-                    Logica::Types::Stream::BufferFunctions::ReadI64 readi64_f
-                );
+                    void AssignReadI64(
+                        Logica::Types::Stream::Buffer::ReadI64Function readi64
+                    );
 
-                void AssignUserData(
-                    void* userdata
-                );
+                    void AssignUserData(
+                        void* userdata
+                    );
 
-                /* The Buffer Functions: */
-                void Init();
-                
-                void SetPosition(
-                    const Logica::Types::Basic::Size position
-                );
-                
-                Logica::Types::Basic::Size GetPosition();
-                
-                void Close();
+                    /* The Buffer Functions: */
+                    void Init();
+                    
+                    void SetPosition(
+                        const Logica::Types::Basic::Size position
+                    );
+                    
+                    Logica::Types::Basic::Size GetPosition();
+                    
+                    void Close();
 
-                Logica::Types::Basic::U8 ReadU8();
+                    Logica::Types::Basic::U8 ReadU8();
 
-                Logica::Types::Basic::U16 ReadU16();
-                
-                Logica::Types::Basic::U32 ReadU32();
-                
-                Logica::Types::Basic::U64 ReadU64();
-                
-                Logica::Types::Basic::I8 ReadI8();
-                
-                Logica::Types::Basic::I16 ReadI16();
-                
-                Logica::Types::Basic::I32 ReadI32();
-                
-                Logica::Types::Basic::I64 ReadI64();
+                    Logica::Types::Basic::U16 ReadU16();
+                    
+                    Logica::Types::Basic::U32 ReadU32();
+                    
+                    Logica::Types::Basic::U64 ReadU64();
+                    
+                    Logica::Types::Basic::I8 ReadI8();
+                    
+                    Logica::Types::Basic::I16 ReadI16();
+                    
+                    Logica::Types::Basic::I32 ReadI32();
+                    
+                    Logica::Types::Basic::I64 ReadI64();
+                private:
+                    Logica::Types::Stream::Buffer::InitFunction         init;
+                    Logica::Types::Stream::Buffer::SetPositionFunction  set_position;
+                    Logica::Types::Stream::Buffer::GetPositionFunction  get_position;
+                    Logica::Types::Stream::Buffer::CloseFunction        close;
+                    Logica::Types::Stream::Buffer::ReadU8Function       readu8;
+                    Logica::Types::Stream::Buffer::ReadU16Function      readu16;
+                    Logica::Types::Stream::Buffer::ReadU32Function      readu32;
+                    Logica::Types::Stream::Buffer::ReadU64Function      readu64;
+                    Logica::Types::Stream::Buffer::ReadI8Function       readi8;
+                    Logica::Types::Stream::Buffer::ReadI16Function      readi16;
+                    Logica::Types::Stream::Buffer::ReadI32Function      readi32;
+                    Logica::Types::Stream::Buffer::ReadI64Function      readi64;
+                    void*                                               userdata;
             };
         };
     };
--- Logica/src/Logica/Types/Stream/Buffer.cpp
+++ Logica/src/Logica/Types/Stream/Buffer.cpp
@@ -3,161 +3,42 @@
 /*
  * Assign Functions:
  */
+#define MergeNames(x, y) x##y
+#define AssignFunctionFactory(uppercase_n, lowercase_n, type)                                       \
+    void Logica::Types::Stream::Buffer::Assign##uppercase_n(                                        \
+        Logica::Types::Stream::Buffer::type lowercase_n                                             \
+    )                                                                                               \
+    {                                                                                               \
+        this->lowercase_n = lowercase_n;                                                            \
+    }                                                                                               
 
-void Logica::Types::Stream::Buffer::AssignInit(
-    Logica::Types::Stream::BufferFunctions::Init init_f
-)
-{
-    this->init_f = init_f;
-}
+AssignFunctionFactory(Init, init, InitFunction);
+AssignFunctionFactory(Close, close, CloseFunction);
+AssignFunctionFactory(GetPosition, get_position, GetPositionFunction);
+AssignFunctionFactory(SetPosition, set_position, SetPositionFunction);
+AssignFunctionFactory(ReadU8, readu8, ReadU8Function);
+AssignFunctionFactory(ReadU16, readu16, ReadU16Function);
+AssignFunctionFactory(ReadU32, readu32, ReadU32Function);
+AssignFunctionFactory(ReadU64, readu64, ReadU64Function);
+AssignFunctionFactory(ReadI8, readi8, ReadI8Function);
+AssignFunctionFactory(ReadI16, readi16, ReadI16Function);
+AssignFunctionFactory(ReadI32, readi32, ReadI32Function);
+AssignFunctionFactory(ReadI64, readi64, ReadI64Function);
+void Logica::Types::Stream::Buffer::AssignUserData(void* userdata)                                  { this->userdata = userdata; }
 
-void Logica::Types::Stream::Buffer::AssignSetPosition(
-    Logica::Types::Stream::BufferFunctions::SetPosition set_position_f
-)
-{
-    this->set_position_f = set_position_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignGetPosition(
-    Logica::Types::Stream::BufferFunctions::GetPosition get_position_f
-)
-{
-    this->get_position_f = get_position_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignClose(
-    Logica::Types::Stream::BufferFunctions::Close close_f
-)
-{
-    this->close_f = close_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadU8(
-    Logica::Types::Stream::BufferFunctions::ReadU8 readu8_f
-)
-{
-    this->readu8_f = readu8_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadU16(
-    Logica::Types::Stream::BufferFunctions::ReadU16 readu16_f
-)
-{
-    this->readu16_f = readu16_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadU32(
-    Logica::Types::Stream::BufferFunctions::ReadU32 readu32_f
-)
-{
-    this->readu32_f = readu32_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadU64(
-    Logica::Types::Stream::BufferFunctions::ReadU64 readu64_f
-)
-{
-    this->readu64_f = readu64_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadI8(
-    Logica::Types::Stream::BufferFunctions::ReadI8 readi8_f
-)
-{
-    this->readi8_f = readi8_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadI16(
-    Logica::Types::Stream::BufferFunctions::ReadI16 readi16_f
-)
-{
-    this->readi16_f = readi16_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadI32(
-    Logica::Types::Stream::BufferFunctions::ReadI32 readi32_f
-)
-{
-    this->readi32_f = readi32_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignReadI64(
-    Logica::Types::Stream::BufferFunctions::ReadI64 readi64_f
-)
-{
-    this->readi64_f = readi64_f;
-}
-
-void Logica::Types::Stream::Buffer::AssignUserData(
-    void* userdata
-)
-{
-    this->userdata = userdata;
-}
-
-/*
- * "API Functions" are declared here:
+/**
+ * Connectors are defined here:
  */
 
-void Logica::Types::Stream::Buffer::Init()
-{
-    this->init_f(this->userdata);
-}
-
-void Logica::Types::Stream::Buffer::SetPosition(
-    const Logica::Types::Basic::Size position
-)
-{
-    this->set_position_f(this->userdata, position);
-}
-
-Logica::Types::Basic::Size Logica::Types::Stream::Buffer::GetPosition()
-{
-    Logica::Types::Basic::Size value = this->get_position_f(this->userdata);
-    return value;
-}
-
-void Logica::Types::Stream::Buffer::Close()
-{
-    this->close_f(this->userdata);
-}
-
-Logica::Types::Basic::U8 Logica::Types::Stream::Buffer::ReadU8()
-{
-    return this->readu8_f(this->userdata);
-}
-
-Logica::Types::Basic::U16 Logica::Types::Stream::Buffer::ReadU16()
-{
-    return this->readu16_f(this->userdata);
-}
-
-Logica::Types::Basic::U32 Logica::Types::Stream::Buffer::ReadU32()
-{
-    return this->readu32_f(this->userdata);
-}
-
-Logica::Types::Basic::U64 Logica::Types::Stream::Buffer::ReadU64()
-{
-    return this->readu64_f(this->userdata);
-}
-
-Logica::Types::Basic::I8 Logica::Types::Stream::Buffer::ReadI8()
-{
-    return this->readi8_f(this->userdata);
-}
-
-Logica::Types::Basic::I16 Logica::Types::Stream::Buffer::ReadI16()
-{
-    return this->readi16_f(this->userdata);
-}
-
-Logica::Types::Basic::I32 Logica::Types::Stream::Buffer::ReadI32()
-{
-    return this->readi32_f(this->userdata);
-}
-
-Logica::Types::Basic::I64 Logica::Types::Stream::Buffer::ReadI64()
-{
-    return this->readi64_f(this->userdata);
-}
+void Logica::Types::Stream::Buffer::Init()                                                          { this->init(this->userdata); }
+void Logica::Types::Stream::Buffer::SetPosition(const Logica::Types::Basic::Size position)          { this->set_position(this->userdata, position); }
+Logica::Types::Basic::Size Logica::Types::Stream::Buffer::GetPosition()                             { return this->get_position(this->userdata); }
+void Logica::Types::Stream::Buffer::Close()                                                         { this->close(this->userdata);              }
+Logica::Types::Basic::U8 Logica::Types::Stream::Buffer::ReadU8()                                    { return this->readu8(this->userdata);      }
+Logica::Types::Basic::U16 Logica::Types::Stream::Buffer::ReadU16()                                  { return this->readu16(this->userdata);     }
+Logica::Types::Basic::U32 Logica::Types::Stream::Buffer::ReadU32()                                  { return this->readu32(this->userdata);     }
+Logica::Types::Basic::U64 Logica::Types::Stream::Buffer::ReadU64()                                  { return this->readu64(this->userdata);     }
+Logica::Types::Basic::I8 Logica::Types::Stream::Buffer::ReadI8()                                    { return this->readi8(this->userdata);      }
+Logica::Types::Basic::I16 Logica::Types::Stream::Buffer::ReadI16()                                  { return this->readi16(this->userdata);     }
+Logica::Types::Basic::I32 Logica::Types::Stream::Buffer::ReadI32()                                  { return this->readi32(this->userdata);     }
+Logica::Types::Basic::I64 Logica::Types::Stream::Buffer::ReadI64()                                  { return this->readi64(this->userdata);     }
